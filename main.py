217dd77d753391833e6a71e172f19081c38c5880
L140-beep <kartoshka0302@mail.ru>
2022-12-18 21:33:36 +0800

    1800 score

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import pyautogui
 import matplotlib.pyplot as plt
 import time
 
-pyautogui.PAUSE = 0.00001
 
 def prepareImage(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -20,36 +19,45 @@ def findObject(image):
     return False
 
 x = 470
-y = 330
-width = 140
-height = 1
+y = 300
+width = 30
+height = 38
 
-cactus_monitor = {"top": y, "left": x, "width": width, "height": height}
+monitor = {"top": y, "left": x, "width": width, "height": height}
 
-yb = 297
-b_width = 50
-bird_monitor = {"top": yb, "left": x, "width": b_width, "height": height}
+x_diff = 130
+width_diff = 100
+monitor1 = {"top": y, "left": x + x_diff, "width": width + width_diff, "height": height}
 
+start_time = time.time()
+
+ctime = 0
 
 with mss.mss() as sct:
     while True:
         jump = False
-        # bird = False
-        image = np.array(sct.grab(monitor=cactus_monitor))
-        # image_bird = np.array(sct.grab(monitor=bird_monitor))
+        image = np.array(sct.grab(monitor=monitor))
+        image1 = np.array(sct.grab(monitor=monitor1))
         
         cactus = prepareImage(image) 
-        # image_bird = prepareImage(image_bird)
+        image1 = prepareImage(image1)
         
-        part1 = cactus[0][:45]
-        part2 = cactus[0][100:]
         
-        # if findObject(image_bird):
-        #     pyautogui.press('down')
-        #     bird = True
-        if findObject(part1):
+        if findObject(cactus):
                 pyautogui.press('up')
                 jump = True
                 
-        if findObject(part2) and jump:
+        if findObject(image1) and jump:
                 pyautogui.press('down')
+                
+        current_time = int(time.time() - start_time)
+        
+        if ctime != current_time:
+            
+            if current_time % 10 == 0:
+                x += (current_time // 10) * 2
+                x_diff += 5
+                width_diff += 10
+                monitor = {"top": y, "left": x, "width": width, "height": height}
+                monitor1 = {"top": y, "left": x + x_diff, "width": width + width_diff, "height": height}
+            ctime = current_time
